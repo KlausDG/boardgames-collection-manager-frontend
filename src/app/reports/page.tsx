92f238a6810b839b9ee77cc737bcf550dba53d30
Klaus <idd_kgalm@uolinc.com>
2024-07-04 17:15:36 -0300

    feat: BoardgamePricesTable

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { BoardgamePricesTable } from "@/modules/boardgame-prices";
 import { MechanicsTable } from "@/modules/mechanics";
 
 export default function Reports() {
   return (
     <>
       <MechanicsTable />
+      <BoardgamePricesTable />
     </>
   );
 }
