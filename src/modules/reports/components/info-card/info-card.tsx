6d8d01f7c16f38f776d99ee9fb04cd5cac3a9ff3
Klaus <idd_kgalm@uolinc.com>
2024-07-18 11:30:42 -0300

    feat: Wight report module

--- src/modules/reports/components/info-card/info-card.tsx
+++ src/modules/reports/components/info-card/info-card.tsx
@@ -5,10 +5,11 @@ import { Box, Paper, Skeleton, Typography } from "@mui/material";
 type InfoCardProps = {
   title: string;
   data: string | number | undefined;
+  shouldFormatData?: boolean;
 };
 
-export const InfoCard = ({ title, data }: InfoCardProps) => {
-  const dataToDisplay = typeof data === "number" ? data.toFixed(2) : data;
+export const InfoCard = ({ title, data, shouldFormatData = false }: InfoCardProps) => {
+  const dataToDisplay = typeof data === "number" && shouldFormatData ? data.toFixed(2) : data;
 
   return (
     <Paper elevation={4}>
@@ -16,6 +17,7 @@ export const InfoCard = ({ title, data }: InfoCardProps) => {
         <Typography variant="subtitle1" fontSize={14}>
           {title}
         </Typography>
+
         {!data ? (
           <Skeleton variant="text" width={28} sx={{ fontSize: "0.875rem" }} />
         ) : (
--- src/modules/reports/views/collection-reports/collection-reports.tsx
+++ src/modules/reports/views/collection-reports/collection-reports.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { CardContainer } from "@/components";
-import { Stack } from "@mui/material";
+import { Box, Stack, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material";
 
 import { InfoCard } from "../../components";
 import { useWeightReport } from "../../hooks";
@@ -11,10 +11,32 @@ export const CollectionReports = () => {
 
   return (
     <CardContainer>
-      <Stack direction="row" spacing={2}>
-        <InfoCard title="Min" data={data?.min} />
-        <InfoCard title="Max" data={data?.max} />
-        <InfoCard title="Avg" data={data?.avg} />
+      <Stack spacing={2}>
+        <Box>
+          <Typography variant="h6">Weight</Typography>
+          <Stack direction="row" spacing={2}>
+            <InfoCard title="Min" data={data?.min} shouldFormatData />
+            <InfoCard title="Max" data={data?.max} shouldFormatData />
+            <InfoCard title="Avg" data={data?.avg} shouldFormatData />
+          </Stack>
+        </Box>
+
+        <Box>
+          <Typography variant="h6">Boardgames by weight</Typography>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                {!!data &&
+                  Object.keys(data?.countInRange).map((value) => <TableCell>{value.replace("_", " - ")}</TableCell>)}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                {!!data && Object.values(data?.countInRange).map((value) => <TableCell>{value}</TableCell>)}
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Box>
       </Stack>
     </CardContainer>
   );
